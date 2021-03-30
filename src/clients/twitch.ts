93db9d3a6aa9c6c6f07d7a700d258b2b6b2bf635
Adam Crowder <adam@adamcrowder.net>
2021-03-29 17:51:10 -0700

    Add cmds/functionality to always announce streams from set twitch channels [regardless of game]
    Signed-off-by: Adam Crowder <adam@adamcrowder.net>

--- src/clients/twitch.ts
+++ src/clients/twitch.ts
@@ -1,5 +1,5 @@
 import { Config } from './configuration';
-import { ApiClient } from 'twitch';
+import { ApiClient, HelixStream } from 'twitch';
 import { RefreshableAuthProvider, StaticAuthProvider } from 'twitch-auth';
 
 export class TwitchApi {
@@ -11,15 +11,35 @@ export class TwitchApi {
   public static client = new ApiClient({ authProvider: TwitchApi.AuthProvider });
 
   public static async getStreamsOfGames(twitchGameIds: string[]) {
-    return (await TwitchApi.client.helix.streams.getStreamsPaginated({ game: twitchGameIds }).getAll()).map((stream) => {
-      return {
-        id: stream.id,
-        title: stream.title,
-        game: stream.gameName,
-        user: stream.userDisplayName,
-        start: stream.startDate,
-        url: `https://twitch.tv/${stream.userName}`,
-      };
-    });
+    if (twitchGameIds.length === 0) return [];
+    return (await TwitchApi.client.helix.streams.getStreamsPaginated({ game: twitchGameIds }).getAll()).map(apiStreamToStreamObj);
   }
+
+  public static async getStreamsOfUsers(twitchUserIds: string[]) {
+    if (twitchUserIds.length === 0) return [];
+    return (await TwitchApi.client.helix.streams.getStreamsPaginated({ userId: twitchUserIds }).getAll()).map(apiStreamToStreamObj);
+  }
+
+  // returns an empty string if not found
+  public static async getTwitchUserID(twitchChannelName: string) {
+    const user = await TwitchApi.client.helix.users.getUserByName(twitchChannelName.toLowerCase());
+    if (!user) return '';
+    return user.id;
+  }
+
+  public static async getTwitchUserNames(twitchIDs: string[]) {
+    const users = await TwitchApi.client.helix.users.getUsersByIds(twitchIDs);
+    return users.map((user) => user.displayName);
+  }
+}
+
+function apiStreamToStreamObj(stream: HelixStream) {
+  return {
+    id: stream.id,
+    title: stream.title,
+    game: stream.gameName,
+    user: stream.userDisplayName,
+    start: stream.startDate,
+    url: `https://twitch.tv/${stream.userName}`,
+  };
 }
---
+++ src/listeners/chat/shared/liveChannel.ts
@@ -0,0 +1,49 @@
+import { TwitchApi } from '../../../clients/twitch';
+import { CommonAdminCommand } from './common';
+import { AnnounceChannel } from '../../../models/announceChannel';
+
+export const addTwitchStreamLiveChannel: CommonAdminCommand = {
+  cmd: 'addtwitchstreamlivechannel',
+  shortDescription: 'Add a twitch channel which should always be announced when going live (regardless of the game)',
+  usageInfo: `usage: addtwitchstreamlivechannel <twitch_username>
+  example: addtwitchstreamlivechannel teambn`,
+  handler: async (ctx, user, param) => {
+    if (!param) return 'Invalid syntax. Please provide a twitch channel username';
+    const twitchID = await TwitchApi.getTwitchUserID(param);
+    if (!twitchID) return `Could not find a twitch channel named '${param}'`;
+    const existingChannel = await AnnounceChannel.getChannel(twitchID);
+    if (existingChannel && existingChannel.announceTypes.has('stream')) return `Twitch channel '${param}' is already being announced for all livestreams!`;
+    await AnnounceChannel.addNewStreamChannel(twitchID);
+    return `Twitch channel '${param}' will have all livestreams announced`;
+  },
+};
+
+export const removeTwitchStreamLiveChannel: CommonAdminCommand = {
+  cmd: 'removetwitchstreamlivechannel',
+  shortDescription: 'Remove a twitch channel so that non-mmbn streams from the channel are not announced',
+  usageInfo: `usage: removetwitchstreamlivechannel <twitch_username>
+  example: removetwitchstreamlivechannel teambn`,
+  handler: async (ctx, user, param) => {
+    if (!param) return 'Invalid syntax. Please provide a twitch channel username';
+    const twitchID = await TwitchApi.getTwitchUserID(param);
+    if (!twitchID) return `Could not find a twitch channel named '${param}'`;
+    const channel = await AnnounceChannel.getChannel(twitchID);
+    if (!channel || !channel.announceTypes.has('stream')) return `Twitch channel '${param}' is not being announced for all livestreams!`;
+    channel.announceTypes.delete('stream');
+    if (channel.announceTypes.size === 0) await channel.remove();
+    else await channel.save();
+    return `Twitch channel '${param}' will no longer be announced for all livestreams`;
+  },
+};
+
+export const listTwitchStreamLiveChannel: CommonAdminCommand = {
+  cmd: 'listtwitchstreamlivechannels',
+  shortDescription: 'List the twitch channels whose livestreams will always be announced regardless of the game',
+  usageInfo: `usage: listtwitchstreamlivechannels`,
+  handler: async () => {
+    const channels = await AnnounceChannel.getStreamDetectionChannels();
+    const twitchUsernames = await TwitchApi.getTwitchUserNames(channels.map((chan) => chan.channel));
+    if (twitchUsernames.length === 0) return 'There are no twitch channels currently set to announce all livestreams';
+    return `The following twitch channels will have all livestreams announced regardless of game: ${twitchUsernames.join(', ')}`;
+  },
+};
